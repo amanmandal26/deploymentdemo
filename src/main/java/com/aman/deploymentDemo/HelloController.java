f319d61aaa1c142f248838b6766aa28447349cec
AmaMandal <127779099+AmaMandal@users.noreply.github.com>
2026-02-14 10:52:02 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/aman/deploymentDemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.aman.deploymentDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot Deployment 🚀";
+    }
+}
+
---
+++ src/test/java/com/aman/deploymentDemo/DeploymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.deploymentDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
